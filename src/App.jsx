1d9e91590e52479cbe1930aed550404f79cf0408
JulienChevreauPro <julienchevreau33@gmail.com>
2024-04-10 08:59:35 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import MyCard from "./components/card.jsx"
+import "./style.scss";
+
+function App() {
+
+  return (
+    <>
+      <MyCard/>
+    </>
+  )
+}
+
+export default App;
+// I choose scss with sass for my test card because the nesting looks convenient to me...
---
+++ src/components/card.jsx
@@ -0,0 +1,18 @@
+function MyCard() {
+
+    return (
+      <main>
+        <header>
+            <img src="src\assets\images\photo_cocktail_sunset.webp" alt="Cocktails at sunset" />
+            <h2>Who wants to be there?</h2>
+        </header>
+        <p>We can't do anything about the sun, but if you need some drinks 🍹, you can count on our new app :</p>
+        <h1>Cocktail explorer</h1>
+        <footer>
+            <p>Soon available on <img src="src\assets\images\logo_github.png" alt="GitHub"/></p>
+        </footer>
+      </main>
+    )
+  }
+
+export default MyCard;
